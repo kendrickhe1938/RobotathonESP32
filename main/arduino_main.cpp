2fa447fb35d94f6168a5c68f2f4300c0e03878e6
Adrian Alberto Sanchez <adriansanchez2374@gmail.com>
2025-10-15 22:18:50 -0500

    code tiaht owwork

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -2,49 +2,77 @@
 // Copyright 2021 Ricardo Quesada
 // http://retro.moe/unijoysticle2
 
+
+
+
 #include "sdkconfig.h"
 #include <Arduino.h>
 #include <Bluepad32.h>
 #include <uni.h>
 #include "controller_callbacks.h"
 
+
+
+
 #define IN1 0
 #define IN2 4
-#define IN3 15
-#define IN4 2
+#define IN3 2
+#define IN4 15
+
+
+
 
 extern ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+
+
 
 void foo(ControllerPtr myController) {
     while(1) {
         BP32.update();
-        if(myController->axisY() <= -1) { //FORWARD
+        if(myController->x()) { //FORWARD
+           
             digitalWrite(IN1, HIGH); // turn on motor
             digitalWrite(IN4, LOW);
-            // digitalWrite(IN3, HIGH);
-            // digitalWrite(IN2, LOW);
+            digitalWrite(IN3, HIGH);
+            digitalWrite(IN2, LOW);
+           
+
+
         }
 
-        else if(myController->axisY() >= 1) { // BACK
+
+
+
+        else if(myController->b()) { // BACK
             digitalWrite(IN1, LOW); // turn on motor
             digitalWrite(IN4, HIGH);
             digitalWrite(IN3, LOW);
             digitalWrite(IN2, HIGH);
         }
 
-        else if(myController->axisX() >= 1) { // RIGHT
+
+
+
+        else if(myController->a()) { // RIGHT
             digitalWrite(IN1, LOW); // turn on motor
-            digitalWrite(IN4, HIGH);
+            digitalWrite(IN4, LOW);
             digitalWrite(IN3, HIGH);
+            digitalWrite(IN2, HIGH);
+        }
+
+
+
+
+        else if(myController->y()) {
+            digitalWrite(IN1, HIGH); // turn on motor
+            digitalWrite(IN4, HIGH);
+            digitalWrite(IN3, LOW);
             digitalWrite(IN2, LOW);
         }
 
-        else if(myController->axisX() <= -1) {
-            digitalWrite(IN1, HIGH); // turn on motor
-            digitalWrite(IN4, LOW);
-            digitalWrite(IN3, LOW);
-            digitalWrite(IN2, HIGH);
-        }
+
+
 
         else {
             digitalWrite(IN1, LOW); // turn on motor
@@ -55,6 +83,9 @@ void foo(ControllerPtr myController) {
         }
     }
 }
+
+
+
 
 void dumpGamepad(ControllerPtr ctl) {
     Console.printf(
@@ -75,26 +106,32 @@ void dumpGamepad(ControllerPtr ctl) {
     );
 }
 
+
+
+
 void setup() {
     BP32.setup(&onConnectedController, &onDisconnectedController);
-    BP32.forgetBluetoothKeys(); 
+    BP32.forgetBluetoothKeys();
     esp_log_level_set("gpio", ESP_LOG_ERROR); // suppress info log spam from gpio_isr_service
     uni_bt_allowlist_set_enabled(true);
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
-    
-    
+   
+   
 }
+
+
+
 
 void loop() {
     vTaskDelay(1); // ensures WDT does not get triggered when no controller is connected
-    BP32.update(); 
+    BP32.update();
     for (auto myController : myControllers) { // only execute code when controller is connected
         if (myController && myController->isConnected() && myController->hasData()) {        
             foo(myController);
-            
+           
             dumpGamepad(myController); // prints the gamepad state, delete or comment if don't need
         }
     }
